5074443b40befe3f3467122fa69bb08cc8b41c83
Vadim <vadim.kurn@gmail.com>
2020-08-26 21:15:44 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+public class Post {
+    private int postId;
+    private String postDate;
+    private String postText;
+    private String communityName;
+    private String communityLink;
+    private String communityLogo;
+    private int postLikes;
+    private int postComments;
+    private int postReposts;
+    private int postViews;
+    // + get/set на все поля
+}
